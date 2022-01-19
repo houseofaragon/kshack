4c7479afdbc49a19808cb9373ca7cd7224152151
houseofaragon <saintkaren@gmail.com>
2022-01-19 18:32:57 -0500

    feat(about) add submission form

--- src/components/FormApp.vue
+++ src/components/FormApp.vue
@@ -19,12 +19,13 @@
             </div>
         </form>
         <div>
-            <div v-show="filteredEntries && filteredEntries.length > 0">
+            <div v-show="entries && entries.length > 0">
                 <form @submit.prevent="filterEntries">
                     <label for="searchQuery">Search:</label><br/>
                     <input id="searchQuery" type="text" v-model="searchQuery" />
                     <button class="active" type="submit">Submit</button>
                 </form>
+                <h3>Search by Category <input id="searchQuery" type="text" v-model="searchQuery" /></h3>
             </div>
             <h3 v-show="entries && entries.length > 0">
                 Sort titles by:
@@ -79,7 +80,6 @@ export default defineComponent({
       activeClass: 'active',
       showLoader: false,
       entries: [] as Entry[],
-      filteredEntries: [] as Entry[],
       columns: [] as string[],
       searchQuery: ''
     }
@@ -92,22 +92,22 @@ export default defineComponent({
       const { count, entries } = responseData.data
       this.response = count
       this.entries = entries
-      this.filteredEntries = entries
       this.columns = Object.keys(entries[0])
     },
     sortAsc () {
-      return this.filteredEntries.sort((a, b) => a.API > b.API ? 1 : -1)
+      return this.entries.sort((a, b) => a.API > b.API ? 1 : -1)
     },
     sortDesc () {
-      return this.filteredEntries.sort((a, b) => a.API < b.API ? 1 : -1)
-    },
-    filterEntries () {
-      console.log(this.searchQuery)
+      return this.entries.sort((a, b) => a.API < b.API ? 1 : -1)
+    }
+  },
+  computed: {
+    filteredEntries () {
       if (!this.searchQuery) {
-        this.filteredEntries = this.entries
-        return
+        return this.entries
       }
-      this.filteredEntries = this.entries.filter(entry => entry.Category === this.searchQuery)
+      const filter = new RegExp(this.searchQuery, 'i')
+      return this.entries.filter(entry => entry.Category.match(filter))
     }
   }
 })
